91f145cc163ba3bf9ad193fcd5be53f1e4e9e1c4
Imran Ali-Shakh <imranalishah829@gmail.com>
2025-01-28 14:52:00 +0200

    implement dict for game state

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
 import os
 import random
-from typing import Dict, List, Optional
+from typing import Dict, List, Set, Union, Optional
 
 ASSETS_FILE = "assets.txt"
 DICTIONARY_FILE = "dict.txt"
+HANGMAN_STAGES = {
+    1: "FIRST_TRY",
+    2: "SECOND_TRY",
+    3: "THIRD_TRY",
+    4: "FOURTH_TRY",
+    5: "FIFTH_TRY",
+    6: "SIXTH_TRY"
+}
+LAST_HANGMAN_ATTEMPT = 6
+
+UNKNOWN_STATE_CHAR = "X"
+START_COUNTER = 0
+GAME_STATE_WON = "You are the winner!"
+GAME_STATE_LOST = "Oh no, you've lost! The correct word was: {word}. Better luck next time!"
+GAME_STATE_NOT_FINISHED = "Game is NOT FINISHED!"
+
+game_state: Dict[str, Union[Dict[str,str], str, List[str], Set[str], int]] = {
+    "assets": None,
+    "random_word": None,
+    "masked_word": None,
+    "guessed_letters": None,
+    "attempts_counter": None
+}
 
 def load_ascii_assets(file_name: str) -> Dict[str, str]:
     """
@@ -40,33 +63,77 @@ def get_random_word(file_name: str) -> str:
 def start_game_round():
     """
     get game option
-    make set up: load assets and get random word
+    make set up for game_state
     if new game start game loop
-    make start_game_loop
+    and after start_game_loop
     """
     game_option = input("[N]ew game or [E]xit\n").lower()
     if game_option == 'n':
         print("Let's start!")
-        ascii_assets = load_ascii_assets(ASSETS_FILE)
-        random_word = get_random_word(DICTIONARY_FILE)
-        start_game_loop(ascii_assets, random_word)
-    elif game_option == 'e':
+        game_state["assets"] = load_ascii_assets(ASSETS_FILE)
+        game_state["random_word"] = get_random_word(DICTIONARY_FILE)
+        game_state["masked_word"] = [UNKNOWN_STATE_CHAR for _ in range(len(game_state["random_word"]))]
+        game_state["guessed_letters"] = set()
+        game_state["attempts_counter"] = START_COUNTER
+        start_game_loop(game_state)
+    else:
         print("Have a nice day!")
 
-def start_game_loop(assets: Dict[str, str], random_word: str):
+def start_game_loop(game_state):
+    while(check_game_state(game_state) == GAME_STATE_NOT_FINISHED):
+        player_guess = make_player_guess(game_state["guessed_letters"])
+        print_state(game_state, player_guess)
+    else:
+        print(check_game_state(game_state))
+
+
+def check_game_state(game_state) -> str:
     """
-    make check_game_state via counter for dict of try and get needed asset for print
-    while loop were we get asking for guessing char of the random word
-    and make rendering of hidden random word and counter for getting suitable
-    asset in sequence of tries
-    and make constants where we print if player win or lose and asking for next try
+    constant for dict of tries
+    counter for key from constant dict
+    if counter == 6 return Game state lost with format string
+    while counter up just return game state not finished
     """
+    if game_state["attempts_counter"] == LAST_HANGMAN_ATTEMPT:
+        return GAME_STATE_LOST.format(word=game_state["random_word"])
+    elif are_all_chars_guessed(game_state["masked_word"]):
+        return GAME_STATE_WON
+    
+    return GAME_STATE_NOT_FINISHED
+
+def make_player_guess(guessed_letters: Set[str]) -> str:
+    while True:
+        player_guess = input("Please, input a letter\n").strip().lower()
+
+        if len(player_guess) != 1 or not player_guess.isalpha():
+            print("Invalid input. Please enter a single letter.")
+            continue
+        elif player_guess in guessed_letters:
+            print(f"You've already guessed '{player_guess}'. Try a different letter")
+            continue
+
+        guessed_letters.add(player_guess)
+        return player_guess
+
+    
+def print_state(game_state, player_guess: str):
+    """
+    I need get counter of attempts and print suitable hangman stage and how many remain
     key_to_fetch = "SECOND_TRY"
     fallback_value = "ASCII asset not found"
     result: Optional[str] = assets.get(key_to_fetch, fallback_value)
     print(result)
     print(f"Random word: {random_word}")
+    """
+    # print(''.join(game_state["masked_word"]))
+    # print(game_state["random_word"].upper())
+    print(game_state["random_word"])
+    print(f"that's your guessing: {", ".join(game_state["guessed_letters"])}")
+    game_state["attempts_counter"] += 1
+    print(player_guess)
 
+def are_all_chars_guessed(masked_word: List[str]):
+    return UNKNOWN_STATE_CHAR not in masked_word
 
 if __name__ == "__main__":
     start_game_round()
